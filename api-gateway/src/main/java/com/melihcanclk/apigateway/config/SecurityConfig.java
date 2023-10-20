d7002a3c8a369d85e01039fe39e02412dc75fff3
Melihcan Cilek <melihcanclk@hotmail.com>
2023-10-20 04:51:13 +0300

    feat(swagger-ui): swagger initialization for microservices and api gateway entegration.

--- api-gateway/src/main/java/com/melihcanclk/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/melihcanclk/apigateway/config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package com.melihcanclk.apigateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -21,8 +18,16 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers("/eureka/**")
-                                .permitAll()
+                        exchange.pathMatchers("/eureka/**").permitAll()
+                                .pathMatchers("/actuator/health/**").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/webjars/**").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/swagger-resources/**").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/inventory-service/v3/api-docs/**").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/notification-service/v3/api-docs/**").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/order-service/v3/api-docs/**").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/product-service/v3/api-docs/**").permitAll()
                                 .anyExchange()
                                 .authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
@@ -30,4 +35,16 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/product-service/v3/api-docs").and().method(HttpMethod.GET)
+                        .uri("lb://product-service"))
+                .route(r -> r.path("/order-service/v3/api-docs").and().method(HttpMethod.GET)
+                        .uri("lb://order-service"))
+                .route(r -> r.path("/inventory-service/v3/api-docs").and().method(HttpMethod.GET)
+                        .uri("lb://inventory-service"))
+                .build();
+    }
 }
--- inventory-service/src/main/java/com/melihcanclk/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/melihcanclk/inventoryservice/controller/InventoryController.java
@@ -2,6 +2,11 @@ package com.melihcanclk.inventoryservice.controller;
 
 import com.melihcanclk.inventoryservice.dto.InventoryResponse;
 import com.melihcanclk.inventoryservice.service.InventoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +20,24 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
+    @Operation(summary = "Inventory Check", description = "Checks the inventory of the given sku code",
+            security = @SecurityRequirement(name = "security_auth"))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Server Error")
+    })
     @GetMapping("/{skuCode}")
     @ResponseStatus(HttpStatus.OK)
     public boolean checkInventory(@PathVariable("skuCode") String skuCode) {
         return inventoryService.checkInventory(skuCode);
     }
 
+    @Operation(summary = "Inventory Check", description = "Checks the inventory of the given sku codes",
+            security = @SecurityRequirement(name = "security_auth"))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Server Error")
+    })
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<InventoryResponse> checkInventory(@RequestParam("skuCode") List<String> skuCodeList) {
